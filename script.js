5b00fa9a6c10dc3336ad424315dabce65b7f07fb
Scott C. Reynolds <scott@scottcreynolds.com>
2016-06-29 15:15:06 -0400

    rewrite

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-'use strict';
+console.log(this === window);
+// returns true
 
-debugger;
+function checkThis(){
+  'use strict';
+  console.log(this);
+}
+checkThis();
+// outputs Window object
+//
+function Chair(style, color) {
+  console.log(this);
+  this.style = style;
+  this.color = color;
+}
+sofa = new Chair("sofa", "green");
+
+var couch = {
+  color: 'green',
+  f: function() {
+    return this;
+  }
+};
+
+console.log(couch.f());
+// outputs object
